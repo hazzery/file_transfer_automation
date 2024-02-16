52ae0d8d330ce0094d4fee98041867e8ee7d780b
Harry Parkes <harrydparkes@proton.me>
2024-02-16 13:02:22 +1300

    error handling in ftp util functions

--- file_transfer_automation/ftp.py
+++ file_transfer_automation/ftp.py
@@ -1,11 +1,24 @@
+import sys
 from typing import Any
+import logging
 import ftplib
+import socket
+
+
+logger = logging.getLogger(__name__)
 
 
 def authenticate(ftp: ftplib.FTP, config: dict[str, Any]):
-    ftp.connect(config['hostname'], config['port_number'])
-    ftp.login(config['username'], config['password'])
-    ftp.cwd(config['working_directory'])
+    try:
+        ftp.connect(config['hostname'], config['port_number'])
+        ftp.login(config['username'], config['password'])
+        ftp.cwd(config['working_directory'])
+    except (socket.gaierror, ConnectionRefusedError) as error:
+        logger.error(error)
+        sys.exit('Failure to connect to FTP server')
+    except ftplib.error_perm as error:
+        logger.error(error)
+        sys.exit(f'Failed to cd into {config["working_directory"]}')
 
 
 def file_names(ftp: ftplib.FTP):
